2635d4ccaad017e3bdce62968933f20e27824825
brunandrade <bruna.card.andrade@gmail.com>
2022-08-24 21:00:15 -0300

    dbContext and docker initial setup

---
+++ CloudWeather.Precipitation/DataAccess/PrecipDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CloudWeather.Precipitation.DataAccess
+{
+    public class PrecipDbContext : DbContext
+    {
+        public PrecipDbContext()
+        {
+
+        }
+        public PrecipDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Precipitation> Precipitation { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            SankeCaseIdentityTableNames(modelBuilder);
+        }
+
+        private static void SankeCaseIdentityTableNames(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Precipitation>(b => { 
+                b.ToTable("precipitation"); 
+            });
+        }
+        
+    }
+}
--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -1,6 +1,35 @@
+using CloudWeather.Precipitation.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Add Database Context
+builder.Services.AddDbContext<PrecipDbContext>(
+    options => {
+        options.EnableSensitiveDataLogging();
+        options.EnableDetailedErrors();
+        options.UseNpgsql(builder.Configuration.GetConnectionString("AppDb"));
+    }, ServiceLifetime.Transient
+);
 
 // Add services to the container.
 var app = builder.Build();
+
+//Add Service Routes
+app.MapGet("/observation/{zip}", async (string zip, [FromQuery] int? days, PrecipDbContext db) =>
+{
+    if(days == null || days < 1 || days > 30)
+    {
+        return Results.BadRequest("Please provide a 'days' query parameter between 1 and 30");
+    }
+
+    var startDate = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
+    var results = await db.Precipitation
+        .Where(precip => precip.ZipCode == zip && precip.CreatedOn > startDate)
+        .ToListAsync();
+
+    return Results.Ok(results);
+});
 
 app.Run();
